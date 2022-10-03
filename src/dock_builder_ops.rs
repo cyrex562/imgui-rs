e70e61148f729ea3363a78c00e1a36d0ecbc8399
azrael <azrael@cpmo.tech>
2022-10-03 13:48:11 -0400

    porting 2022 oct 03 * direction.rs * dock_builder_ops.rs * dock_context.rs

--- src/dock_builder_ops.rs
+++ src/dock_builder_ops.rs
@@ -1,7 +1,11 @@
-// inline * mut ImGuiDockNode   DockBuilderGetCentralNode(ImGuiID node_id)              
-pub fn DockBuilderGetCentralNode(node_id: ImGuiID) -> *mut ImGuiDockNode
-{ 
-    let mut node = DockBuilderGetNode(node_id); 
-    if !node {return null_mut();} 
-    return DockNodeGetRootNode(node).CentralNode; 
+use std::ptr::null_mut;
+use crate::dock_node::ImGuiDockNode;
+use crate::dock_node_ops::DockNodeGetRootNode;
+use crate::type_defs::ImGuiID;
+
+// inline * mut ImGuiDockNode   DockBuilderGetCentralNode(ImGuiID node_id)
+pub fn DockBuilderGetCentralNode(node_id: ImGuiID) -> *mut ImGuiDockNode {
+    let mut node = DockBuilderGetNode(node_id);
+    if !node { return null_mut(); }
+    return DockNodeGetRootNode(node).CentralNode;
 }
--- src/dock_context.rs
+++ src/dock_context.rs
@@ -2,12 +2,12 @@ use crate::dock_node_settings::ImGuiDockNodeSettings;
 use crate::dock_request::ImGuiDockRequest;
 use crate::storage::ImGuiStorage;
 
-#[derive(Default,Debug,Clone)]
-pub struct ImGuiDockContext
-{
-pub Nodes:  ImGuiStorage,          // Map ID -> ImGuiDockNode*: Active nodes
-pub Requests:  Vec<ImGuiDockRequest>,
-pub NodesSettings:  Vec<ImGuiDockNodeSettings>,
-pub WantFullRebuild:  bool,
+#[derive(Default, Debug, Clone)]
+pub struct ImGuiDockContext {
+    pub Nodes: ImGuiStorage,
+    // Map ID -> ImGuiDockNode*: Active nodes
+    pub Requests: Vec<ImGuiDockRequest>,
+    pub NodesSettings: Vec<ImGuiDockNodeSettings>,
+    pub WantFullRebuild: bool,
     // ImGuiDockContext()              { memset(this, 0, sizeof(*this)); }
 }
