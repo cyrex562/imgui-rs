0e4cfc55050fe0bdc4767ddfb65fe7e246b92992
cyrex <cyrex562@gmail.com>
2022-10-09 11:12:59 -0400

    ported clipboard_ops.rs

--- src/clipboard_ops.rs
+++ src/clipboard_ops.rs
@@ -1,15 +1,21 @@
 #![allow(non_snake_case)]
 
+use crate::string_ops::str_to_const_c_char_ptr;
+use crate::GImGui;
+use libc::c_char;
+
 // *const char GetClipboardText()
-pub fn GetClipboardText() -> *const c_char
-{
+pub unsafe fn GetClipboardText() -> *const c_char {
     let g = GImGui; // ImGuiContext& g = *GImGui;
-    return if g.IO.GetClipboardTextFn { g.IO.GetClipboardTextFn(g.IO.ClipboardUserData) }else { "" };
+    return if g.IO.GetClipboardTextFn {
+        g.IO.GetClipboardTextFn(g.IO.ClipboardUserData)
+    } else {
+        str_to_const_c_char_ptr("")
+    };
 }
 
 // c_void SetClipboardText(*const char text)
-pub fn SetClipboardText(text: *const c_char)
-{
+pub unsafe fn SetClipboardText(text: *const c_char) {
     let g = GImGui; // ImGuiContext& g = *GImGui;
     if g.IO.SetClipboardTextFn {
         g.IO.SetClipboardTextFn(g.IO.ClipboardUserData, text);
